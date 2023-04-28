993433d0f79118cebd6d0267365f8155017c3844
a96lex <a96lex@gmail.com>
2023-04-28 10:27:01 +0200

    change: add time in between requests
    to look more human

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ import helpers from "./helpers.js";
 console.log("BambooHR automatic time entry");
 console.log("📆 Getting past month times");
 
-const times = await api.getTimesheet()
-const days = helpers.parseDates(times)
+const times = await api.getTimesheet();
+const days = helpers.parseDates(times);
 
-days.forEach(async day => {
-    const { start, end } = helpers.getRandomTimes()
-    console.log("⏰ Clocking in and out for " + day + " from " + start + " to " + end);
-    await api.addTimeEntry({ day, start, end })
+days.forEach(async (day, idx) => {
+    const { start, end } = helpers.getRandomTimes();
+    setTimeout(async () => {
+        console.log(
+            "⏰ Clocking in and out for " + day + " from " + start + " to " + end
+        );
+        await api.addTimeEntry({ day, start, end });
+    }, idx * (4000 + 500 * Math.random()));
 });
 
 console.log("🎉 all done!");
